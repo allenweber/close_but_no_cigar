366a219c1a12d9dd8592f5bda62c496a5e8a9da8
allenweber <weberallen1@gmail.com>
2017-01-31 10:35:19 -0800

    Basic drawing works, returns "Yes"

--- close.rb
+++ close.rb
@@ -1,10 +1,37 @@
 def drawing(raffle, my_num)
-
-	raffle.include?(my_num)
-	# if false
-	# 	do raffle.each - 1
-	# else 
-	# 	return true
-	# end
-
+	raffle.include?(my_num)	
+	if true
+		puts "Yes"
+		return true
+	else return false
+	end
 end
+
+
+
+
+
+
+	# 		else return false
+	# 		end
+	# 		if false + 2
+	# 		raffle.include?(my_num)
+	# 	do raffle.each - 1
+	# 	end
+	# 	return true
+	# else 
+	# end
+	# if false
+
+
+# 			return true
+# 		end
+# 		if my_num == ticket
+# 	end
+# 	winner
+# 	winner == true
+# 	winner.each do |ticket|
+# def winner (my_num, *raffle)
+# def winner (my_num, raffle)
+# end
+# end
--- close_test.rb
+++ close_test.rb
@@ -4,8 +4,19 @@ require_relative "close.rb"
 class Test_winning_numbers < Minitest::Test
 
 	def test_1_return
-			raffle = [1553, 3754, 4999, 7890, 9999, 8234, 2221, 4242, 6969]
-			my_num = 5020
+			raffle = ["1553", "3754", "4999", "7890", "9999", "8234", "2221", "4242", "6969"]
+			my_num = "4999"
 		assert_equal(true, drawing(raffle, my_num))
 	end
+
+	def 
+	end	
+		
+	end
+
+	# def test_2_return_true
+	# 		raffle = [1553, 3754, 4999, 7890, 9999, 8234, 2221, 4242, 6969]		
+	# 		my_num = 4999
+	# 	assert_equal(true, winner(raffle, my_num))
+	# end
 end
